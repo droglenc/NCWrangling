c2605cc744b12c89f5265610fe857560be994004
Derek Ogle <derek@derekogle.com>
2021-07-24 12:02:10 -0500

    again

--- modules/Relational-Data/CE_1.R
+++ modules/Relational-Data/CE_1.R
@@ -8,12 +8,12 @@ B <- data.frame(uniq=c("A14","A15","A16","A17"),
                 meas3=c("am","pm","am","pm"))
 
 knitr::kable(A,align="c",caption="Data frame A") %>%
-  kable_classic(full_width=FALSE,html_font="Cambria",position="float_left") %>%
-  kable_styling(bootstrap_options=c("hover","condensed","striped")) %>%
+  kable_classic(full_width=FALSE,html_font="Cambria") %>%
+  kable_styling(bootstrap_options=c("hover","condensed"),position="float_left") %>%
   row_spec(0,bold=TRUE)
 knitr::kable(B,align="c",caption="Data frame B") %>%
-  kable_classic(full_width=FALSE,html_font="Cambria",position="right") %>%
-  kable_styling(bootstrap_options=c("hover","condensed","striped")) %>%
+  kable_classic(full_width=FALSE,html_font="Cambria") %>%
+  kable_styling(bootstrap_options=c("hover","condensed"),position="right") %>%
   row_spec(0,bold=TRUE)
 
 Z <- inner_join(A,B,by="uniq")
@@ -31,4 +31,4 @@ knitr::kable(Z,align="c") %>%
   row_spec(0,bold=TRUE)
 
 
-# Script created at 2021-07-24 11:54:26
+# Script created at 2021-07-24 12:00:35
