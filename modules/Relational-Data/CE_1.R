436b9dd074090dc70afebac2dd92715f1583e3a6
Derek Ogle <derek@derekogle.com>
2021-07-24 12:34:05 -0500

    again

--- modules/Relational-Data/CE_1.R
+++ modules/Relational-Data/CE_1.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-library(kableExtra)
 
 A <- data.frame(uniq=c("A13","A15","A15","A16","A16"),
                 meas1=c(34,87,65,33,12),
@@ -7,28 +6,19 @@ A <- data.frame(uniq=c("A13","A15","A15","A16","A16"),
 B <- data.frame(uniq=c("A14","A15","A16","A17"),
                 meas3=c("am","pm","am","pm"))
 
-knitr::kable(A,align="c",caption="Data frame A") %>%
-  kable_classic(full_width=FALSE,html_font="Cambria") %>%
-  kable_styling(bootstrap_options=c("hover","condensed"),position="float_left") %>%
-  row_spec(0,bold=TRUE)
-knitr::kable(B,align="c",caption="Data frame B") %>%
-  kable_classic(full_width=FALSE,html_font="Cambria") %>%
-  kable_styling(bootstrap_options=c("hover","condensed"),position="right") %>%
-  row_spec(0,bold=TRUE)
-
 Z <- inner_join(A,B,by="uniq")
-
-knitr::kable(Z,align="c") %>%
-  kable_classic(full_width=FALSE,html_font="Cambria") %>%
-  kable_styling(bootstrap_options=c("hover","condensed","striped")) %>%
-  row_spec(0,bold=TRUE)
 
 Y <- semi_join(A,B,by="uniq")
 
-knitr::kable(Z,align="c") %>%
-  kable_classic(full_width=FALSE,html_font="Cambria") %>%
-  kable_styling(bootstrap_options=c("hover","condensed","striped")) %>%
-  row_spec(0,bold=TRUE)
+X <- right_join(A,B,by="uniq")
+
+W <- anti_join(A,B,by="uniq")
+
+V <- full_join(A,B,by="uniq")
+
+U <- left_join(A,B,by="uniq")
+
+T <- left_join(B,A,by="uniq")
 
 
-# Script created at 2021-07-24 12:07:23
+# Script created at 2021-07-24 12:33:40
